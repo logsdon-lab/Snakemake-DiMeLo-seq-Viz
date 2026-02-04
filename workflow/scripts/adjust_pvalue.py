2c4cd62b1f649bf56cb37051f7d749561d6feaaf
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2026-02-04 11:04:32 -0500

    Override schema so polars doesn't infer type incorrectly.

--- workflow/scripts/adjust_pvalue.py
+++ workflow/scripts/adjust_pvalue.py
@@ -9,7 +9,16 @@ def main():
     cohen_h_lower_bound = float(sys.argv[3])
     p_value = float(sys.argv[4])
 
-    df_dmr = pl.read_csv(infile, has_header=True, separator="\t")
+    df_dmr = pl.read_csv(
+        infile,
+        has_header=True,
+        separator="\t",
+        schema_overrides={
+            "a_pct_modified": pl.Float64,
+            "b_pct_modified": pl.Float64,
+            "effect_size": pl.Float64,
+        },
+    )
     p_values_adj = false_discovery_control(df_dmr["map_pvalue"])
     (
         df_dmr.with_columns(map_pvalue_adj=pl.Series(p_values_adj))
